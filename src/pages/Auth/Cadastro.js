9eb73e6f78082b0c2653ffaf05ebe3b472ba188d
Hiram Epifanio <hiramepifanio1@gmail.com>
2023-06-01 00:36:19 -0300

    Crianto tela de cadastro

--- src/pages/Auth/Cadastro.js
+++ src/pages/Auth/Cadastro.js
@@ -1,8 +1,28 @@
 import './Auth.css'
 
+import { Link } from 'react-router-dom'
+
 const Cadastro = () => {
+
+  const handleSubmit = (e) => {
+    e.preventDefault();
+  }
+
   return (
-    <div>Cadastro</div>
+    <div>
+      <h2>veetask</h2>
+      <p className='subtitle'>Cadastre-se para começar a gerenciar as suas tarefas.</p>
+      <form onSubmit={handleSubmit}>
+        <input type="text" placeholder='Nome' />
+        <input type="email" placeholder='E-mail' />
+        <input type="password" placeholder='Senha' />
+        <input type="password" placeholder='Confirme a senha' />
+        <input type="submit" placeholder='Cadastrar' />
+      </form>
+      <p>
+        Já tem conta? <Link to='/login'>Entrar.</Link>
+      </p>
+    </div>
   )
 }
 
